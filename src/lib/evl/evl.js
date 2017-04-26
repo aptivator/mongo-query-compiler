7024d0fb7557f866c9ad1613c3007ef64a814eae
aptivator <aptivator@gmail.com>
2017-04-26 13:35:59 -0400

    Create operations aggregator

--- src/lib/evl/evl.js
+++ src/lib/evl/evl.js
@@ -1,12 +1,5 @@
-import _                    from 'lodash';
-import browser              from 'object-browser';
-import arrayOperations      from './operations/array/array';
-import freeFormOperations   from './operations/free-form/free-form';
-import primitiveOperations  from './operations/primitive/primitive';
-import referencerNormalizer from './lib/referencer-normalizer';
-
-let operations = _.extend({}, arrayOperations, freeFormOperations, primitiveOperations);
-operations = referencerNormalizer(operations);
+import browser    from 'object-browser';
+import operations from './lib/operations-aggregator';
 
 export default (o, path, op, testValue, symbolTable, symbolName) => {
   let loperand = browser(o, path, {exists: true});
