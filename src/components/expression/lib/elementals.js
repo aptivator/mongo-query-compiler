aa80fee70e5ebe596bc05d57ef839f029cad4e6e
aptivator <aptivator@gmail.com>
2017-04-26 14:49:47 -0400

    Spin off documentation into a separate repo

--- src/components/expression/lib/elementals.js
+++ src/components/expression/lib/elementals.js
@@ -3,4 +3,4 @@ import arrayOps     from '../../../lib/evl/operations/array/array';
 import freeFormOps  from '../../../lib/evl/operations/free-form/free-form';
 import primitiveOps from '../../../lib/evl/operations/primitive/primitive';
 
-export default [].concat(_.keys(arrayOps), _.keys(freeFormOps), _.keys(primitiveOps));
+export default _.keys(arrayOps).concat(_.keys(freeFormOps), _.keys(primitiveOps));
