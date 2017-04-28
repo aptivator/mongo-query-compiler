81a53406954066f5c0f2553f2ea34a936c29aea9
aptivator <aptivator@gmail.com>
2017-04-28 14:54:38 -0400

    Add more  tests

--- test/tests/utility/flatten.js
+++ test/tests/utility/flatten.js
@@ -13,6 +13,30 @@ describe('$flatten', () => {
     expect(results.length).to.equal(1);
   });
   
+  it('can be placed anywhere in the query', () => {
+    let query = compiler({
+      $flatten: true,
+      store: {
+        employees: {
+          name: {
+            $and: {
+              first: {
+                $and: {
+                  $or: {
+                    $eq: /^j/i
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+    
+    let results = data.filter(query);
+    expect(results.length).to.equal(1);
+  });
+  
   it('works with $ref', () => {
     let query = compiler({'tags.2': {$in: {$ref: 'store.employees.name.first', $flatten: true}}});
     let results = data.filter(query);
